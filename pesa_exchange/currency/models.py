29b4b2d1ac03d46026a6c5428e53e904a3a61eef
jimmy <jimmy.aduvagah@gmail.com>
2021-06-23 04:06:56 +0300

    modify transaction method

--- pesa_exchange/currency/models.py
+++ pesa_exchange/currency/models.py
@@ -14,29 +14,13 @@ class Currency(AbstractBase):
 
 
 def get_user_currency_rate(currency):
-    api_key = "d870a9424ea744069b50cebbd3ebcf84"
+    coinoxr.app_id = "d870a9424ea744069b50cebbd3ebcf84"
     rates = coinoxr.Latest().get(base="USD")
     rates = rates.body['rates']
     return rates.get(currency.currency_iso_code, 0)
 
 
-def get_user_currency_amount(rate, amount):
-    if rate > 1:
-        return amount * rate
-    elif rate < 1:
-        return amount / rate
-    else:
-        return amount
+def get_usd_amount(rate, amount):
+    return amount * rate
 
 
-def get_receiver_transfer_amount(rate, sender_amount, receiver_currency):
-    usd_amount = 0
-    if rate > 1:
-        usd_amount = sender_amount / rate
-    elif rate < 1:
-        usd_amount = sender_amount * rate
-    else:
-        usd_amount = sender_amount
-
-    receiver_rate = get_user_currency_rate(receiver_currency)
-    return get_user_currency_amount(receiver_rate, usd_amount)
